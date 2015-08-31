d2c8677e4e0c1d8f6e58e955e192a59183f29894
Danny van Kooten <dannyvankooten@gmail.com>
2015-08-31 17:57:58 +0700

    A no-dependencies PHP 5.2 compatible version.

--- add-plain-text-email.php
+++ add-plain-text-email.php
@@ -5,20 +5,95 @@ Plugin URI: http://dannyvankooten.com/wordpress-plugins/mailchimp-for-wordpress/
 Description: Adds a text/plain email to text/html emails to decrease the chance of emails being tagged as spam.
 Version: 1.1
 Author: Danny van Kooten
-Author URI: http://dannyvankooten.com
+Author URI: http://dannyvanKooten.com
 */
 
 defined( 'ABSPATH' ) OR exit;
 
-// only run on PHP 5.3+
-if( version_compare( PHP_VERSION, '5.3', '>=' ) ) {
+class APTE {
 
-	// include autoloader (only once)
-	require_once __DIR__ . '/vendor/autoload.php';
+	/**
+	 * @var string
+	 */
+	protected $previous_altbody;
 
-	// create instance of class
-	$modifier = new APTE_Email_Modifier();
-	$modifier->add_hooks();
+	/**
+	 * Add hooks
+	 */
+	public function add_hooks() {
+		// add action so function actually runs
+		add_action('phpmailer_init', array( $this, 'set_plaintext_body' ) );
+	}
+
+	/**
+	 * @param PHPMailer $phpmailer
+	 */
+	public function set_plaintext_body( $phpmailer ) {
+
+		// don't run if sending plain text email already
+		if( $phpmailer->ContentType === 'text/plain' ) {
+			return;
+		}
+
+		// don't run if altbody is set (by other plugin)
+		if( ! empty( $phpmailer->AltBody ) && $phpmailer->AltBody !== $this->previous_altbody ) {
+			return;
+		}
+
+		// set AltBody
+		$text_message = $this->strip_html_tags( $phpmailer->Body );
+		var_dump( $text_message ); die();
+		$phpmailer->AltBody = $text_message;
+		$this->previous_altbody = $text_message;
+	}
+
+	/**
+	 * Remove HTML tags, including invisible text such as style and
+	 * script code, and embedded objects.  Add line breaks around
+	 * block-level tags to prevent word joining after tag removal.
+	 */
+	private function strip_html_tags( $text ) {
+	    $text = preg_replace(
+	        array(
+	          // Remove invisible content
+	            '@<head[^>]*?>.*?</head>@siu',
+	            '@<style[^>]*?>.*?</style>@siu',
+	            '@<script[^>]*?.*?</script>@siu',
+	            '@<object[^>]*?.*?</object>@siu',
+	            '@<embed[^>]*?.*?</embed>@siu',
+	            '@<applet[^>]*?.*?</applet>@siu',
+	            '@<noframes[^>]*?.*?</noframes>@siu',
+	            '@<noscript[^>]*?.*?</noscript>@siu',
+	            '@<noembed[^>]*?.*?</noembed>@siu',
+		        '@\t+@siu',
+		        '@\n+@siu'
+	        ),
+	        '',
+	        $text );
+
+		// replace certain elements with a line-break
+		$text = preg_replace(
+			array(
+				'@</?((div)|(h[1-9])|(tr)|(p)|(pre))@iu'
+			),
+			"\n\$0",
+			$text );
+
+		// replace other elements with a space
+		$text = preg_replace(
+			array(
+				'@</?((td)|(th))@iu'
+			),
+			" \$0",
+			$text );
+
+		// strip all remaining HTML tags
+	    $text = strip_tags( $text );
+		
+		return $text;
+	}
+
 }
 
-
+$apte = new APTE;
+$apte->add_hooks();
